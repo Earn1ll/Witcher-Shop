63db3ca7b0501b6fe6a49f40c915f71cdf95a2ef
Earn1ll <coldsteelblr@gmail.com>
2022-07-24 14:57:51 +0300

    overlay v0.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 function Wrapper() {
     return (
         <div className="wrapper">
+            <div className="overlay">
+                <div className="drawer">
+                    <h2>Корзина</h2>
+                    <div className="cartItem">
+                        <img src="img/items/1.jpg" alt="item" width={80} />
+                        <div>
+                            <p>Фигурка Геральта</p>
+                            <b>12999 руб.</b>
+                        </div>
+                        <img src="img/remove.svg" alt="remove" width={20} height={20}/>
+                    </div>
+                </div>
+            </div>
             <header>
                 <div className="headerLeft">
                     <img src="/img/the-witcher-001.png" alt="logo" width={40} height={40}/>
@@ -20,9 +33,18 @@ function Wrapper() {
                 </ul>
             </header>
             <div className="content">
-                <h1>Все аксессуары</h1>
+                <div className="contentItem">
+                    <h1>Все аксессуары</h1>
+                    <div className="search-block">
+                        <img src="img/search.svg" alt="search" width={18} height={18} />
+                        <input type="text" placeholder="Поиск..." />
+                    </div>
+                </div>
                 <div className="items">
                     <div className="card">
+                        <div className="favorite">
+                            <img src="img/heart.svg" alt="heart" width={20} height={20}/>
+                        </div>
                         <img src="img/items/1.jpg" alt="item" width={145} height={200}/>
                         <h5>Фигурка Геральта</h5>
                         <div className="cardBottom">
@@ -36,6 +58,9 @@ function Wrapper() {
                         </div>
                     </div>
                     <div className="card">
+                        <div className="favorite">
+                            <img src="img/heart.svg" alt="heart" width={20} height={20}/>
+                        </div>
                         <img src="img/items/4.jpg" alt="item" width={165} height={200}/>
                         <h5>Значек</h5>
                         <div className="cardBottom">
@@ -49,6 +74,9 @@ function Wrapper() {
                         </div>
                     </div>
                     <div className="card">
+                        <div className="favorite">
+                            <img src="img/heart.svg" alt="heart" width={20} height={20}/>
+                        </div>
                         <img src="img/items/2.jpg" alt="item" width={145} height={200}/>
                         <h5>Кулон</h5>
                         <div className="cardBottom">
@@ -62,6 +90,9 @@ function Wrapper() {
                         </div>
                     </div>
                     <div className="card">
+                        <div className="favorite">
+                            <img src="img/heart.svg" alt="heart" width={20} height={20}/>
+                        </div>
                         <img src="img/items/3.jpg" alt="item" width={145} height={200}/>
                         <h5>Кольцо</h5>
                         <div className="cardBottom">

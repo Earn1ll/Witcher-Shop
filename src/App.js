e9d8c49cec5ebc4d7aa5df2e0b172cd1aa3f3a2a
Earn1ll <coldsteelblr@gmail.com>
2022-08-01 13:32:29 +0300

    card component refactoring(add Map)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+    { title: 'Фигурка Геральта', price: 2999, imageUrl:"/img/items/1.jpg" },
+    { title: 'Медальен волка', price: 899, imageUrl:"/img/items/2.jpg"  },
+    { title: 'Кольцо серебрянное с печаткой волка', price: 4999, imageUrl:"/img/items/3.jpg" },
+    { title: 'значек', price: 699, imageUrl:"/img/items/4.jpg" },
+    ];
 
 function Wrapper() {
     return (
@@ -13,12 +19,19 @@ function Wrapper() {
                 <div className="contentItem">
                     <h1>Все аксессуары</h1>
                     <div className="search-block">
-                        <img src="img/search.svg" alt="search" width={18} height={18} />
-                        <input type="text" placeholder="Поиск..." />
+                        <img src="img/search.svg" alt="search" width={18} height={18}/>
+                        <input type="text" placeholder="Поиск..."/>
                     </div>
                 </div>
                 <div className="items">
-                    <Card/>
+
+
+                    {arr.map((obj) => (
+                        <Card title={obj.title}
+                              price={obj.price}
+                              imageUrl={obj.imageUrl}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
 
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
             <div className="favorite">
                 <img src="img/heart.svg" alt="heart" width={20} height={20}/>
             </div>
-            <img src="img/items/1.jpg" alt="item" width={145} height={200}/>
-            <h5>Фигурка Геральта</h5>
+            <img src={props.imageUrl} alt="item" width={145} height={200}/>
+            <h5>{props.title}</h5>
             <div className="cardBottom">
                 <div className="cardPrice">
                     <span>Цена:</span>
-                    <b>12999 руб.</b>
+                    <b>{props.price}</b>
                 </div>
                 <button className="button">
                     <img src="img/plus.svg" alt="add" width={15} height={15}/>
